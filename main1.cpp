762d4177447cec403d43400263e9490acd765a59
AiselMohamed <91670688+AiselMohamed@users.noreply.github.com>
2022-12-19 13:13:50 +0200

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
-#include <bits/stdc++.h>
- using namespace std;
- long  long a,b,c,ans=0;
- cin>>
-  long long mn=min({a,b,c});
+using namespace std;
+class car{
+    string *name;
+    int *model;
+    int *price;
+    int *speed;
+    string *color;
+public:
+    car(){
+        name= new string ;
+        model= new int;
+        price = new int ;
+        speed = new int;
+        color = new string ;
+        cout<<"enter car info :  "<<endl;
+        cout<<"car name :"; cin>>*name;
+        cout<<"car model :"; cin>>*model;
+        cout<<"car price :"; cin>>*price;
+        cout<<"car speed :"; cin>>*speed;
+        cout<<"car color :"; cin>>*color;
+    }
+    ~car(){
+        delete name;
+        delete model;
+        delete price;
+        delete speed;
+        delete color;
+    }
+    void price_display();
+    friend int increment_price(const car&);
+};
+void car::price_display(){
+    cout<<*price;
+}
+int increment_price( const car& c){
+    int new_price;
+    new_price= *c.price+1000;
+    return new_price;
 }
 
-
+int main() {
+    car c1;
+    cout<<"car old price :";
+    c1.price_display();
+    cout<<"car new price"<<increment_price(c1);
+}
