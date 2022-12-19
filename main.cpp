7028ec89e89162a5f728845a5f39f325cc1c3881
AiselMohamed <91670688+AiselMohamed@users.noreply.github.com>
2022-12-19 13:08:06 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,83 @@
 #include <iostream>
-
 using namespace std;
-class worker {
+class triangle{
 private:
-    string name ;
-    int sal;
+    int *W,*L;
 public:
-    worker(string n,int s){
-        name=n;
-        sal=s;
+    triangle(int w,int l){
+        W=new int ;
+        L=new int;
+
+         *W=w;
+         *L=l;
+    }
+    triangle(){
+        W=new int ;
+        L=new int;
+        cout<<"enter W:";cin>>*W;
+        cout<<"enter L:";cin>>*L;
+
 
     }
-    friend float avg(const worker& w1,const worker& w2,const worker& w3,const worker& w4,const worker& w5);
+    ~triangle(){
+
+        delete W;
+        delete L;
+    }
+    int area(){
+        return *W * *L;
+    }
+    void print();
+    friend void largeArea(triangle r[],int);
 };
-float avg(const worker& w1,const worker& w2,const worker& w3,const worker& w4,const worker& w5){
-   int sum=w1.sal+w2.sal+w3.sal+w4.sal+w5.sal;
-    float avg=(float )sum/5;
-    return avg;
+void triangle:: print(){
+cout<<*W<<*L;
 }
-    int main(){
-        worker w1("aisel",2000),w2("abdullah",3000),w3("mohamed",4000),w4("mostafa",5000),w5("yousef",6000);
-        cout<<avg(w1,w2,w3,w4,w5);
+void largeArea(triangle r[],int size){
+    int max=r[0].area();
+    for (int i = 1; i < size; ++i) {
+        if(r[i].area()>max)
+            max=r[i].area();
+
+
+    }
+    cout<<max;
 }
+int main(){
+const int size=4;
+triangle r[size];
+
+    largeArea(r,size);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
